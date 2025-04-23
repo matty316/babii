77d67cedf4fab4f7cabd8fcd023e21a5c787eef4
matty <mattreed1890@gmail.com>
2025-04-23 13:43:31 -0500

    put cube verts in counter clockwise winding order

--- babii/Renderer.swift
+++ babii/Renderer.swift
@@ -17,7 +17,7 @@ public class Renderer: NSObject, MTKViewDelegate {
     let pipelineState: MTLRenderPipelineState
     let commandQueue: MTLCommandQueue
     var lastTime: Double = CFAbsoluteTimeGetCurrent()
-    let wireframe = true
+    let wireframe = false
     var scene: GameScene
     
     override public init() {
@@ -50,7 +50,7 @@ public class Renderer: NSObject, MTKViewDelegate {
             pipelineStateDescriptor.fragmentFunction = fragmentFunc
             pipelineStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
             pipelineStateDescriptor.depthAttachmentPixelFormat = .depth32Float
-            pipelineStateDescriptor.vertexDescriptor = Self.vertexDescriptor()
+            pipelineStateDescriptor.vertexDescriptor = Vertex.vertexDescriptor()
             
             let pipelineState = try device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
             
@@ -102,24 +102,6 @@ public class Renderer: NSObject, MTKViewDelegate {
             commandBuffer.present(currentDrawable)
         }
         commandBuffer.commit()
-    }
-    
-    static func vertexDescriptor() -> MTLVertexDescriptor {
-        let descriptor = MTLVertexDescriptor()
-        descriptor.attributes[0].format = .float3
-        descriptor.attributes[0].offset = 0
-        descriptor.attributes[0].bufferIndex = 0
-        
-        descriptor.attributes[1].format = .float3
-        descriptor.attributes[1].offset = MemoryLayout<Vertex>.offset(of: \.normal)!
-        descriptor.attributes[1].bufferIndex = 0
-        
-        descriptor.attributes[2].format = .float2
-        descriptor.attributes[2].offset = MemoryLayout<Vertex>.offset(of: \.uv)!
-        descriptor.attributes[2].bufferIndex = 0
-        
-        descriptor.layouts[0].stride = MemoryLayout<Vertex>.stride
-        return descriptor
     }
     
 //    static func depthTexture() -> MTLTextureDescriptor {
