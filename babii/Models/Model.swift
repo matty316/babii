30aa261b5a67dc85c84dab3e1c59ab8727b70ae9
matty <mattreed1890@gmail.com>
2025-06-16 18:07:11 -0500

    add model3d struct

--- babii/Models/Model.swift
+++ babii/Models/Model.swift
@@ -32,6 +32,9 @@ extension Model {
 
 extension MTLVertexDescriptor {
     static func vertexDescriptor() -> MTLVertexDescriptor {
+        return MTKMetalVertexDescriptorFromModelIO(Self.mdlVertexDescriptor())!
+    }
+    static func mdlVertexDescriptor() -> MDLVertexDescriptor {
         let vertexDescriptor = MDLVertexDescriptor()
         var offset = 0
         vertexDescriptor.attributes[0] = MDLVertexAttribute(
@@ -58,6 +61,50 @@ extension MTLVertexDescriptor {
         vertexDescriptor.layouts[0]
         = MDLVertexBufferLayout(stride: offset)
         
-        return MTKMetalVertexDescriptorFromModelIO(vertexDescriptor)!
+        return vertexDescriptor
+    }
+}
+
+struct Model3d: Model {
+    var type: ModelType = .ModelIO
+    
+    var position: SIMD3<Float>
+    
+    var rotationAngle: Float
+    
+    var rotation: SIMD3<Float>
+    
+    var scale: Float
+    
+    let mesh: MTKMesh
+    
+    func render(renderEncoder: MTLRenderCommandEncoder, device: MTLDevice) {
+        renderEncoder.setVertexBuffer(mesh.vertexBuffers[0].buffer, offset: 0, index: 0)
+        for submesh in mesh.submeshes {
+            renderEncoder.drawIndexedPrimitives(
+                type: .triangle,
+                indexCount: submesh.indexCount,
+                indexType: submesh.indexType,
+                indexBuffer: submesh.indexBuffer.buffer,
+                indexBufferOffset: submesh.indexBuffer.offset
+            )
+        }
+    }
+    
+    init(device: MTLDevice, assetName: String, position: SIMD3<Float>, rotationAngle: Float, rotation: SIMD3<Float>, scale: Float) {
+        self.position = position
+        self.rotationAngle = rotationAngle
+        self.rotation = rotation
+        self.scale = scale
+        
+        let allocator = MTKMeshBufferAllocator(device: device)
+        
+        guard let assetUrl = Bundle.main.url(forResource: assetName, withExtension: "usdz") else {
+            fatalError("asset not in main bundle")
+        }
+        
+        let asset = MDLAsset(url: assetUrl, vertexDescriptor: MTLVertexDescriptor.mdlVertexDescriptor(), bufferAllocator: allocator)
+        let mdlMesh = asset.childObjects(of: MDLMesh.self).first as! MDLMesh
+        self.mesh = try! MTKMesh(mesh: mdlMesh, device: device)
     }
 }
